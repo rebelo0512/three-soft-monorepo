7761372489e89a6ed9171c8ac88aead7a673b2c8
Marcelo Eduardo Rebelo <marcelinho_05@live.com>
2022-07-18 17:30:48 -0300

    [core-backend] ✨ add base dto and base repository

---
+++ packages/core-backend/src/domain/dtos/BaseDto.ts
@@ -0,0 +1,4 @@
+export interface BaseDto {
+  created_at: Date;
+  updated_at: Date;
+}
---
+++ packages/core-backend/src/domain/repositories/IBaseRepository.ts
@@ -0,0 +1,7 @@
+export abstract class IBaseRepository<EntityDto> {
+  abstract findAll(): Promise<EntityDto[]>;
+  abstract findById(id: number): Promise<EntityDto>;
+  abstract create(entity: EntityDto): Promise<void>;
+  abstract update(entity: EntityDto): Promise<void>;
+  abstract delete(id: number): Promise<void>;
+}
