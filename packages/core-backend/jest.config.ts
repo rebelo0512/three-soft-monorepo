ceaaa062720645c236bd37d2a6261f1317fea920
Marcelo Eduardo Rebelo <marcelinho_05@live.com>
2022-07-18 17:30:20 -0300

    ✨ add core-backend package

---
+++ packages/core-backend/jest.config.ts
@@ -0,0 +1,14 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  clearMocks: true,
+  coverageProvider: "v8",
+  rootDir: "tests",
+  testRegex: ".*\\..*spec\\.ts$",
+  transform: {
+    "^.+\\.ts?$": ["@swc/jest"],
+  },
+};
