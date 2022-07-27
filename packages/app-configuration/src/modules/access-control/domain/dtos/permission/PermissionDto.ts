00222993e94f84ac6b6a6886558ccf706d858827
Marcelo Eduardo Rebelo <marcelinho_05@live.com>
2022-07-27 18:45:53 -0300

    [core-backend] 🏷️ update permission dto

--- packages/app-configuration/src/modules/access-control/domain/dtos/permission/PermissionDto.ts
+++ packages/app-configuration/src/modules/access-control/domain/dtos/permission/PermissionDto.ts
@@ -1,8 +1,9 @@
-import { BaseDto } from '@three-soft/core-backend';
+import { BaseDto, OmitBaseDto } from '@three-soft/core-backend';
+import { PermissionDomainDto } from './PermissionDomainDto';
 
-export interface PermissionDto extends BaseDto {
+export interface PermissionDto extends BaseDto, OmitBaseDto<PermissionDomainDto> {
   perm_id: number;
   perm_name: string;
-  perm_sub_dom_name: string;
+  perm_sub_dom_name: string | null;
   perm_dom_id: number;
 }
