cfde854663af275b1f21f3cd60c1b275bfd49cf9
Marcelo Eduardo Rebelo <marcelinho_05@live.com>
2022-07-28 11:53:50 -0300

    [app-configuration] ✅ add testst to permission in memory repository

--- packages/app-configuration/src/modules/access-control/domain/repositories/IPermissionRepository.ts
+++ packages/app-configuration/src/modules/access-control/domain/repositories/IPermissionRepository.ts
@@ -1,9 +1,11 @@
 import { IBaseRepository } from '@three-soft/core-backend';
 import { PermissionDto } from '../dtos';
 
+/* c8 ignore start */
 export abstract class IPermissionRepository extends IBaseRepository<PermissionDto> {
   abstract findByDomainName(domain_name: string): Promise<PermissionDto[]>;
   abstract findBySubDomainName(sub_domain: string): Promise<PermissionDto[]>;
   abstract findAllSubDomainsByDomainId(domain_id: number): Promise<PermissionDto[]>;
   abstract findByName(name: string): Promise<PermissionDto>;
 }
+/* c8 ignore stop */
--- packages/app-configuration/tests/helpers/factories/permission.ts
+++ packages/app-configuration/tests/helpers/factories/permission.ts
@@ -1,14 +1,16 @@
-import { generateString } from '@three-soft/core-backend';
-import { PermissionDto, IPermissionRepository } from '../../../src';
+import { generateString, OmitBaseDto } from '@three-soft/core-backend';
+import { PermissionDto, IPermissionRepository, PermissionDomainDto } from '../../../src';
 
 export async function createPermission(repository: IPermissionRepository, id: number, props?: PermissionDto) {
   const permission: PermissionDto = props || {
     perm_id: id,
-    perm_dom_id: 1,
     perm_sub_dom_name: generateString(10),
     perm_name: generateString(10),
     created_at: new Date(),
-    updated_at: new Date()
+    updated_at: new Date(),
+    perm_dom_id: id * 2,
+    perm_dom_name: generateString(10),
+    perm_system_name: generateString(10)
   };
 
   await repository.create(permission);
@@ -16,17 +18,27 @@ export async function createPermission(repository: IPermissionRepository, id: nu
   return permission;
 }
 
-export async function createPermissions(repository: IPermissionRepository, total: number) {
-  const promises = [];
+export async function createPermissions(
+  repository: IPermissionRepository,
+  total: number,
+  permission_domain?: OmitBaseDto<PermissionDomainDto>
+) {
+  const promises: Array<Promise<PermissionDto>> = [];
   for (let index = 0; index < total; index += 1) {
+    const permission_domain_entity = permission_domain || {
+      perm_dom_id: index * 2,
+      perm_dom_name: generateString(10),
+      perm_system_name: generateString(10)
+    };
+
     promises.push(
       repository.create({
         perm_id: index + 1,
-        perm_dom_id: 1,
         perm_sub_dom_name: `Sub Domain ${String(index + 1).padStart(2, '0')}`,
         perm_name: `Name ${String(index + 1).padStart(2, '0')}`,
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        ...permission_domain_entity
       })
     );
   }
