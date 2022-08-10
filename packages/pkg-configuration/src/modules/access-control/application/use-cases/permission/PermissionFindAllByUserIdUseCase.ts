e72795d3c18b848eb9088010d17878854c2b508f
Marcelo Eduardo Rebelo <marcelinho_05@live.com>
2022-08-10 17:22:57 -0300

    [pkg-configuration] ✨|✅ add cache in permission find all by user id user case and tests to it

--- packages/pkg-configuration/src/modules/access-control/application/use-cases/permission/PermissionFindAllByUserIdUseCase.ts
+++ packages/pkg-configuration/src/modules/access-control/application/use-cases/permission/PermissionFindAllByUserIdUseCase.ts
@@ -1,18 +1,34 @@
-import { BaseUseCase, EntityNotFoundError } from '@three-soft/core-backend';
+import { BaseUseCase, EntityNotFoundError, ICacheBaseRepository } from '@three-soft/core-backend';
 import { IPermissionRepository, PermissionDto, PermissionFindAllByUserIdInputDto } from '../../../domain';
 import { IUserRepository } from '../../../../user';
 
 export class PermissionFindAllByUserIdUseCase extends BaseUseCase<PermissionFindAllByUserIdInputDto, string[]> {
-  constructor(private permissionRepository: IPermissionRepository, private userRepository: IUserRepository) {
+  private cache_key = 'CUSTOMER_PERMISSIONS-CUSTOMER_ID';
+
+  constructor(
+    private permissionRepository: IPermissionRepository,
+    private userRepository: IUserRepository,
+    private permissionCacheRepository: ICacheBaseRepository
+  ) {
     super();
   }
 
   async execute(input: PermissionFindAllByUserIdInputDto): Promise<string[]> {
     const user = await this.getUser(input);
 
+    const cache = await this.getCache(input);
+
+    if (cache) {
+      return this.formatCache(cache);
+    }
+
     const permissions = await this.permissionRepository.findAllByGroupId(user.group_id);
 
-    return this.formatPermission(permissions);
+    const permissions_formatted = this.formatPermission(permissions);
+
+    await this.createCache(permissions_formatted);
+
+    return permissions_formatted;
   }
 
   private async getUser(input: PermissionFindAllByUserIdInputDto) {
@@ -21,6 +37,24 @@ export class PermissionFindAllByUserIdUseCase extends BaseUseCase<PermissionFind
     if (!user) throw new EntityNotFoundError('Usuário', input.user_id, 'id');
 
     return user;
+  }
+
+  private async getCache(input: PermissionFindAllByUserIdInputDto) {
+    const cache_key = `${this.cache_key}-${input.user_id}`;
+
+    this.permissionCacheRepository.setKey(cache_key);
+
+    const cache = await this.permissionCacheRepository.getCache();
+
+    return cache;
+  }
+
+  private formatCache(cache: string): string[] {
+    return JSON.parse(cache);
+  }
+
+  private async createCache(permissions_formatted: string[]) {
+    await this.permissionCacheRepository.setCache(JSON.stringify(permissions_formatted), 60 * 60);
   }
 
   private formatPermission(permissions: PermissionDto[]) {
--- packages/pkg-configuration/tests/integration/modules/access-control/application/use-cases/permission/PermissionFindAllByUserIdUseCase.int.spec.ts
+++ packages/pkg-configuration/tests/integration/modules/access-control/application/use-cases/permission/PermissionFindAllByUserIdUseCase.int.spec.ts
@@ -19,10 +19,12 @@ import {
   IUserRepository,
   ICompanyRepository,
   CompanyMysqlRepository,
-  PermissionFindAllByUserIdUseCase
+  PermissionFindAllByUserIdUseCase,
+  PermissionRedisRepository
 } from '../../../../../../../src';
 
 describe('PermissionFindAllByUserIdUseCase Integration Tests', () => {
+  let permissionCacheRepository: PermissionRedisRepository;
   let companyRepository: ICompanyRepository;
   let userRepository: IUserRepository;
   let groupRepository: IGroupRepository;
@@ -31,12 +33,17 @@ describe('PermissionFindAllByUserIdUseCase Integration Tests', () => {
   let findAllByUserIdUseCase: PermissionFindAllByUserIdUseCase;
 
   beforeAll(async () => {
+    permissionCacheRepository = new PermissionRedisRepository();
     companyRepository = new CompanyMysqlRepository();
     userRepository = new UserMysqlRepository();
     groupRepository = new GroupMysqlRepository();
     permissionDomainRepository = new PermissionDomainMysqlRepository();
     repository = new PermissionMysqlRepository();
-    findAllByUserIdUseCase = new PermissionFindAllByUserIdUseCase(repository, userRepository);
+    findAllByUserIdUseCase = new PermissionFindAllByUserIdUseCase(
+      repository,
+      userRepository,
+      permissionCacheRepository
+    );
   });
 
   beforeEach(async () => {
@@ -45,6 +52,7 @@ describe('PermissionFindAllByUserIdUseCase Integration Tests', () => {
 
   afterAll(async () => {
     await DatabaseMysqlConnection.destroy();
+    permissionCacheRepository.getConnection()?.disconnect();
   });
 
   it('should return all permissions by user id', async () => {
@@ -69,6 +77,40 @@ describe('PermissionFindAllByUserIdUseCase Integration Tests', () => {
       permission_one.perm_id,
       permission_two.perm_id
     ]);
+
+    const permissions = await findAllByUserIdUseCase.execute({ user_id: user.user_id });
+
+    expect(permissions.length).toBe(2);
+    expect(permissions).toEqual([
+      `${domain.perm_system_name}.${domain.perm_dom_name}.${permission_one.perm_sub_dom_name}.${permission_one.perm_name}`,
+      `${domain.perm_system_name}.${domain.perm_dom_name}.${permission_two.perm_name}`
+    ]);
+  });
+
+  it('should return all permissions using cache by user id', async () => {
+    const group = await createGroup(groupRepository);
+    const company = await createCompany(companyRepository);
+
+    const user = await createUser(userRepository, { company_id: company.comp_id, group_id: group.group_id });
+
+    const domain = await createPermissionDomain(permissionDomainRepository);
+
+    const permission_one = await createPermission(repository, domain.perm_dom_id, {
+      name: 'Perm 01',
+      sub_domain: 'Sub 01'
+    });
+    const permission_two = await createPermission(repository, domain.perm_dom_id, {
+      name: 'Perm 02',
+      sub_domain: null
+    });
+    await createPermission(repository, domain.perm_dom_id, { name: 'Perm 03', sub_domain: 'Sub 02' });
+
+    await createGroupPermissions(DatabaseMysqlConnection, group.group_id, [
+      permission_one.perm_id,
+      permission_two.perm_id
+    ]);
+
+    await findAllByUserIdUseCase.execute({ user_id: user.user_id });
 
     const permissions = await findAllByUserIdUseCase.execute({ user_id: user.user_id });
 
